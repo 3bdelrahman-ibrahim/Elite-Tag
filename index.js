c640f1eb84afb0aa7aa5cf80eea57c81f84bd1f9
abdelrahman ibrahim <abdelrahman22ibrahim@icloud.com>
2024-09-15 00:05:06 +0300

    .

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-// index.js
 const express = require("express");
 const path = require("path");
-
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 8080;
 
-// Serve static files from the 'public' directory
+// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
+// Rewrite rule for /id/* to serve card.html
+app.get("/id/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "card.html"));
+});
+
+// Handle other routes
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server is running at http://127.0.0.1:${port}`);
 });
